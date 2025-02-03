c9f334703b34fce64e57c48649d4847567c270e7
seyasfina <seiya990127@gmail.com>
2025-02-03 16:55:59 +0900

    スタイルチェックの修正

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,5 @@
 class Video < ApplicationRecord
   has_many :clips, dependent: :destroy
 
-  validates :url, presence: true, uniqueness: true, format: URI::regexp(%w[http https])
-
+  validates :url, presence: true, uniqueness: true, format: URI.regexp(%w[http https])
 end
--- db/migrate/20250202113451_create_clips.rb
+++ db/migrate/20250202113451_create_clips.rb
@@ -3,8 +3,8 @@ class CreateClips < ActiveRecord::Migration[7.2]
     create_table :clips do |t|
       t.references :user, null: false, foreign_key: true
       t.references :video, null: false, foreign_key: true
-      t.float :start_time, null:false
-      t.float :end_time, null:false
+      t.float :start_time, null: false
+      t.float :end_time, null: false
 
       t.timestamps
     end
