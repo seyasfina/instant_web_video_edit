d50d2d7e7e8babae041031dc69dafaa7f787b131
seyasfina <seiya990127@gmail.com>
2025-02-22 03:12:36 +0900

    lintチェックの修正とHTTPステータスの修正

--- test/controllers/clips_controller_test.rb
+++ test/controllers/clips_controller_test.rb
@@ -11,27 +11,27 @@ class ClipsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create clip" do
-    post video_clips_url(@video), params: { clip: { 
-      user_id: @user.id, 
-      video_id: @video.id, 
-      start_time: 1.5, 
-      end_time: 1.5 
+    post video_clips_url(@video), params: { clip: {
+      user_id: @user.id,
+      video_id: @video.id,
+      start_time: 1.5,
+      end_time: 1.5
     } }
-    assert_response :redirect
+    assert_response :created
   end
 
   test "should update clip" do
     patch video_clip_url(@video, @clip), params: { clip: {
-      user_id: @user.id, 
-      video_id: @video.id, 
-      start_time: 2.0, 
-      end_time: 2.5 
+      user_id: @user.id,
+      video_id: @video.id,
+      start_time: 2.0,
+      end_time: 2.5
     } }
-    assert_response :redirect
+    assert_response :success
   end
 
   test "should destroy clip" do
     delete video_clip_url(@video, @clip)
-    assert_response :redirect
+    assert_response :no_content
   end
 end
