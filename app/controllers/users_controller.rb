ae4e85d913bb92478c6b763675127b903ba9a1d3
seyasfina <seiya990127@gmail.com>
2025-02-24 00:47:21 +0900

    lintチェック修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show]
-  before_action :authorize_user, only: [:show]
+  before_action :set_user, only: [ :show ]
+  before_action :authorize_user, only: [ :show ]
 
   def show
   end
