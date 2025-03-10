ac5fed6ffb982c4fe6ce94305f2518582c32a00d
seyasfina <seiya990127@gmail.com>
2025-03-10 18:01:09 +0900

    秒から時間分秒への変換ロジック実装2

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -5,6 +5,19 @@ class Clip < ApplicationRecord
   validates :start_time, :end_time, presence: true
   validate :starttime_must_be_before_endtime
 
+  def seconds_to_hms(seconds)
+    seconds = seconds.to_f
+    h = (seconds / 3600).floor
+    m = ((seconds % 3600) / 60).floor
+    s = seconds % 60
+    
+    if h == 0
+      format("%02d:%05.2f", m, s)
+    else
+      format("%02d:%02d:%05.2f", h, m, s)
+    end
+  end  
+
   private
 
   def starttime_must_be_before_endtime
