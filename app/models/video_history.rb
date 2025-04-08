b85edbda6eae8ebb2a697cef02094008c16223f5
seyasfina <seiya990127@gmail.com>
2025-04-08 16:07:05 +0900

    誤ったテーブル名の修正とモデルの調整

--- app/models/video_history.rb
+++ app/models/video_history.rb
@@ -2,6 +2,7 @@ class VideoHistory < ApplicationRecord
   belongs_to :user
   belongs_to :video
 
+  validates :user_id, uniqueness: { scope: :video_id }
   validates :user_id, presence: true
   validates :video_id, presence: true
 end
