3a7e32105c936576c28de2df05536a0c2647ec95
seyasfina <seiya990127@gmail.com>
2025-05-08 23:25:39 +0900

    lint_check修正

--- test/controllers/video_favorites_controller_test.rb
+++ test/controllers/video_favorites_controller_test.rb
@@ -4,12 +4,12 @@ class VideoFavoritesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @video = videos(:one)
   end
-  
+
   test "should create video_favorite" do
     post video_video_favorite_url(@video), as: :json
     assert_response :success
   end
-  
+
   test "should destroy video_favorite" do
     delete video_video_favorite_url(@video), as: :json
     assert_response :success
