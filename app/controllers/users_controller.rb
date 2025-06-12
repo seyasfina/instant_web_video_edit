b5a687fe5ed3b70ff2048a29f3f6ef2d4dad5414
seyasfina <seiya990127@gmail.com>
2025-06-13 00:21:52 +0900

    Userテーブルのカラム修正と不要な性的ファイルの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: [ :show ]
   before_action :authorize_user, only: [ :show ]
 
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+
   def show
     @video_histories = @user.video_histories.includes(:video).order(last_played_at: :desc)
     @video_favorites = @user.video_favorites.includes(:video).order(created_at: :desc)
