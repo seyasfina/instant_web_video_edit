abedd98aae40ec0c12836fa71f2ed2be04527ff4
seyasfina <seiya990127@gmail.com>
2025-06-23 01:24:41 +0900

    importmapに記載されているファイルの追記

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 import Rails from "@rails/ujs";
 import "controllers"
 import "video_show"
+import "stimulus-autocomplete"
+import "@hotwired/stimulus"
+import "@hotwired/stimulus-loading"
 
 Rails.start();
