f61e97595123b4abca7aaca68d14bc2e01669584
seyasfina <seiya990127@gmail.com>
2025-09-05 15:40:50 +0900

    クリップに通し番号を付与

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -5,6 +5,10 @@ class Clip < ApplicationRecord
   validates :start_time, :end_time, presence: true
   validate :starttime_must_be_before_endtime
 
+  scope :ordered, -> { order(position: :asc, id: :asc) }
+
+  before_create :assign_tail_position
+
   def seconds_to_hms(seconds)
     seconds = seconds.to_f
     h = (seconds / 3600).floor
@@ -24,5 +28,10 @@ class Clip < ApplicationRecord
     if start_time > end_time
       errors.add(:base, "開始時間は終了時間より前に設定してください。")
     end
+  end
+
+  def assign_tail_position
+    max = Clip.where(video_id:, user_id:).maximum(:position) || 0
+    self.position = max + 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_12_120118) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_04_082329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_12_120118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "position", null: false
     t.index ["user_id"], name: "index_clips_on_user_id"
+    t.index ["video_id", "user_id", "position"], name: "index_clips_on_vid_uid_position", unique: true
     t.index ["video_id"], name: "index_clips_on_video_id"
   end
 
