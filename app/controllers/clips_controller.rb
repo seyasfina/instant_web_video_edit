593d5fe88b5c343fae36f37ec19c2e0e4d98c5e9
seyasfina <seiya990127@gmail.com>
2025-09-12 03:39:24 +0900

    クリップの並べ替え実装

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -28,6 +28,19 @@ class ClipsController < ApplicationController
     head :no_content
   end
 
+  def reorder
+    ids = params.permit(order: []).fetch(:order, []).filter_map { |v| Integer(v) rescue nil }
+    clips = @video.clips.where(user: current_user, id: ids).index_by(&:id)
+
+    Clip.transaction do
+      ids.each_with_index do |id, index|
+        if clip = clips[id]
+          clip.update!(position: index + 1)
+        end
+      end
+    end
+  end
+
   private
 
   def set_video
